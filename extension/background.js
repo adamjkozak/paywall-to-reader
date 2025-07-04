becca3eda1e349639198b939a3cd33350c7f5064
Adam Kozak <akozak2@gmail.com>
2025-07-04 08:33:14 -0400

    Strip query parameters before archiving

--- extension/background.js
+++ extension/background.js
@@ -2,6 +2,8 @@ chrome.action.onClicked.addListener(tab => {
   if (!tab || !tab.url) {
     return;
   }
-  const archiveUrl = `https://archive.ph/newest/${encodeURIComponent(tab.url)}`;
+  const urlObj = new URL(tab.url);
+  const baseUrl = `${urlObj.origin}${urlObj.pathname}`;
+  const archiveUrl = `https://archive.ph/newest/${encodeURIComponent(baseUrl)}`;
   chrome.tabs.update(tab.id, { url: archiveUrl });
 });
